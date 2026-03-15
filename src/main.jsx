53864c4d1f23401f928fe2d4fc50c6f2be0c79c2
tuli fisher <fishertuli@gmail.com>
2026-03-15 15:15:49 -0400

    switched to vite so got rid of the basename that was needed for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ initStorage();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='https://tuli-fisher.github.io/'>
+    <BrowserRouter /*basename='https://tuli-fisher.github.io/'*/ >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
